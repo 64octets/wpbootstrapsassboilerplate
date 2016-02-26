f7c8bc48e2b9fa7820a799eb783354fdeb185e1c
Mark <mark.a.suarez@gmail.com>
2016-02-25 22:16:30 -0500

    more flickity

--- front-page.php
+++ front-page.php
@@ -3,9 +3,21 @@
 		<main role="main">
 			<section>
 				<div class="main-gallery">
-					<div class="gallery-cell">lol</div>
-					<div class="gallery-cell">pee</div>
-					<div class="gallery-cell">zee</div>
+					<div class="gallery-cell">
+						<img src="https://d13yacurqjgara.cloudfront.net/users/373338/screenshots/1968797/gambit_dribbble.png" alt="" />
+					</div>
+					<div class="gallery-cell">
+						<img src="https://d13yacurqjgara.cloudfront.net/users/373338/screenshots/1922720/jubilee_dribbble.png" alt="" />
+					</div>
+					<div class="gallery-cell">
+						<img src="https://d13yacurqjgara.cloudfront.net/users/373338/screenshots/1903948/cyclops_dribbble.png" alt="" />
+					</div>
+					<div class="gallery-cell">
+						<img src="https://d13yacurqjgara.cloudfront.net/users/373338/screenshots/1903948/cyclops_dribbble.png" alt="" />
+					</div>
+					<div c;ass="galler-cell">
+						<img src="https://d13yacurqjgara.cloudfront.net/users/373338/screenshots/1891255/weaponx_dribbble.png" alt="" />
+					</div>
   				</div>
 			</section>
 			<section>
